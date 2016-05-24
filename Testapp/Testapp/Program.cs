4ea00c8b3ce701a41465df2ea1e8d1f8596ef0ab
Alexandra Fedotova <feyasasha19111996@gmail.com>
2016-05-25 02:17:25 +0300

    2:added class and Method , called it in Main

--- Testapp/Testapp/Program.cs
+++ Testapp/Testapp/Program.cs
@@ -4,12 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Testapp
+namespace AutoPark
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Auto car = new Auto(1223, "Ford", 1243581);
+            string a = Auto.GetInfo(car);
+            Console.WriteLine(a);
+        }
+    }
+
+    class Auto
+    {
+        private int number;
+        public string model;
+        protected int price;
+
+        public Auto(int number, string model, int price)
+        {
+            this.number = number;
+            this.model = model;
+            this.price = price;
+        }
+
+        public static string GetInfo(Auto a)
+        {
+            return "model" + a.model + ",id=" + a.number + " costs" + a.price;
         }
     }
 }
