1a606b60d402b428765f8bf3f0389addc3987076
glory <glory@gmail.com>
2016-05-25 02:36:12 +0300

    3:added some changes

--- Testapp/Testapp/Program.cs
+++ Testapp/Testapp/Program.cs
@@ -4,12 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Testapp
+namespace AutoPark
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Auto car=new Auto(1223,"Ford",1243581);
+            string a = Auto.GetInformation(car);
+        }
+    }
+
+    class Auto
+    {
+        private int number;
+        public string model;
+        protected int price;
+
+        public Auto (int number, string model,int price)
+        {
+            this.number = number;
+            this.model = model;
+            this.price = price;
+        }
+
+        public static string GetInformation(Auto a)
+        {
+            return "model" + a.model + ",id=" + a.number + " costs" + a.price;
         }
     }
 }
